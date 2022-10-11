a05538eab7fcf8dcb4812562275c3013e6fceee9
Katherine Brower <ktbrower101@gmail.com>
2022-10-11 08:40:02 -0600

    some changes

---
+++ 4_Lab/Assets/TriggerEventBehaviour.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Events;
+public class TriggerEventBehaviour : MonoBehaviour
+{
+   public UnityEvent triggerEnterEvent;
+
+   private void onTriggerEnter(Collider other)
+   {
+      triggerEnterEvent.Invoke();
+   }
+}
